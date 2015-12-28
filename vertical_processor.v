b0c579f55855825c17f6542fa87c4bb0b352a5f1
Tengku Ahmad Madya Putra <tahmadmp@gmail.com>
2015-12-28 18:24:27 +0700

    Create Top Level without UART. Some modifications are made to other design entities.

--- vertical_processor.v
+++ vertical_processor.v
@@ -1,4 +1,7 @@
 module vertical_processor(
+	input clk,
+	input rst,
+	input control_change_row,
 	input[639:0] original,
 	input template,
 	output sad_status,
@@ -6,7 +9,7 @@ module vertical_processor(
 );
 	wire[599:0] sad_flag;
 	wire[9:0] out_sad[639:0];
-	pe processing_element_0(clk, rst, out_sad[0], template, original[0], control_change_row, out_sad[39], 1'b0);
+	pe processing_element_0(clk, rst, out_sad[0], template, original[0], control_change_row, out_sad[39], 10'b0);
 	
 	encoder encdr(
 		.binary_out(coordinate),
@@ -36,7 +39,7 @@ module vertical_processor(
 	generate
 		for (k = 40; k < 639; k = k + 1)
 		begin:out_assign
-			assign sad_flag[k-40] = out_sad[k] == 500 ? 0 : 1; 	
+			assign sad_flag[k-40] = out_sad[k] == 10'd500 ? 1'b0 : 1'b1; 	
 		end
 	endgenerate
 
